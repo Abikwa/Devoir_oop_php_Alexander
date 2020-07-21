89e70b75ac40a3a08351198075612a5e2ad4ac24
ABIKWA Alexandre <Abikalex8@gmail.com>
2020-07-21 23:58:14 +0200

    Ajout creation du constructur et de la methode d'affichge

--- Vehicule/Taxi.php
+++ Vehicule/Taxi.php
@@ -2,5 +2,36 @@
 namespace Devoir_oop_php_Alexandre\Vehicule\Taxi;
 use  Devoir_oop_php_Alexandre\Vehicule\Vehicule\Vehicule;
 class Taxi extends Vehicule{
-    
+    private $model;
+    public function __construct($roue, $color, $typC, $num, $model)
+    {
+        parent::__construct($roue, $color, $typC, $num);
+        $this->model = $model;
+    }
+    public function AffichageTaxi()
+    {
+        # code...
+        $this->AffichageVehicule();
+        echo  "<br>enplus c'est un Taxi du model <b>".$this->model."</b>";
+    }
+
+    /**
+     * Get the value of model
+     */ 
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    /**
+     * Set the value of model
+     *
+     * @return  self
+     */ 
+    public function setModel($model)
+    {
+        $this->model = $model;
+
+        return $this;
+    }
 }
