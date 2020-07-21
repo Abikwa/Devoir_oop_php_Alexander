e3ae32a36cb9cdd279af62b4a6dfa512146bdc97
ABIKWA Alexandre <Abikalex8@gmail.com>
2020-07-21 23:57:00 +0200

    Ajout des taxi

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
     use Devoir_oop_php_Alexandre\Vehicule\Vehicule\Vehicule;
+    use Devoir_oop_php_Alexandre\Vehicule\Taxi\Taxi;
     include_once("Vehicule\Vehicule.php");
+    include_once("Vehicule\Taxi.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,8 +20,19 @@
             <aside class="answer"><!--  pillier dE LA POO-->
                 <?php
                      echo "<span class='question'>Vehicule</span>";
-                    $personne = new Vehicule(4, "blue", "Essence", "28522A");
-                    $personne->AffichageVehicule();
+                    $vehicule = new Vehicule(4, "blue", "Essence", "28522A");
+                    $vehicule->AffichageVehicule();
+                    $vehicule = new Vehicule(12, "Noire", "Mazout", "125-7854-DB");
+                    $vehicule->AffichageVehicule();
+                ?>
+            </aside>
+            <aside class="answer"><!--  pillier dE LA POO-->
+                <?php
+                     echo "<span class='question'>Taxi du type Vehicule</span>";
+                    $taxi = new Taxi(4, "blue", "Essence", "28522A", "SwagMOrtel");
+                    $taxi->AffichageTaxi();
+                    $taxi = new Taxi(12, "Noire", "Mazout", "125-7854-DB", "Hungtolow");
+                    $taxi->AffichageTaxi();
                 ?>
             </aside>
         </article>
