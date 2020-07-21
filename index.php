9cb8b5bf20cdd3c7af1236c5ef9a34b2b37b9ebb
ABIKWA Alexandre <Abikalex8@gmail.com>
2020-07-21 22:32:03 +0200

    commit initial

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    use Devoir_oop_php_Alexandre\Vehicule\Vehicule\Vehicule;
+    include_once("Vehicule\Vehicule.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Devoir_oop_php_Alexandre</title>
+</head>
+<body>
+    <div>
+        <article>
+            <h2>gg</h2>
+            <?php
+                $vehicul = new Vehicule("998", "blue", "essance", "1524A");
+                $vehicul->AffichageVehicule();
+            ?>
+        </article>
+    </div>
+</body>
+</html>
