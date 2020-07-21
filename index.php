d9880e3351a1fd4066750ea722de2bd4134eba89
ABIKWA Alexandre <Abikalex8@gmail.com>
2020-07-21 23:17:20 +0200

    creation des vehicule

--- index.php
+++ index.php
@@ -8,18 +8,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="styleTp1.css" />
+    <link rel="stylesheet" type="text/css" href="styleTp2.css" />
     <title>Devoir_oop_php_Alexandre</title>
 </head>
 <body>
-    <div>
+<div id="corps">
+        <h2 id="titre">Deuxieme travail traitant sur la POO en php</h2>
         <article>
-            <h2>gg</h2>
-            <?php
-                $vehicul = new Vehicule("998", "blue", "essance", "1524A");
-                $vehicul->AffichageVehicule();
-            ?>
+            <aside class="answer"><!--  pillier dE LA POO-->
+                <?php
+                     echo "<span class='question'>Vehicule</span>";
+                    $personne = new Vehicule(4, "blue", "Essence", "28522A");
+                    $personne->AffichageVehicule();
+                ?>
+            </aside>
         </article>
     </div>
+    <footer>
+        <p align="right"><a href="mailto:abikalex8@gmail.com">Fait par ABIKWANINE BATUMIKE Alexandre</a></p>
+    </footer>
+
 </body>
 </html>
