7330b2a467db75f7efc567c164c6f1d2a3cc716f
ABIKWA Alexandre <Abikalex8@gmail.com>
2020-07-21 23:58:42 +0200

    Ajout d'une propriete

--- Vehicule/Vehicule.php
+++ Vehicule/Vehicule.php
@@ -8,7 +8,7 @@ class Vehicule
     private $couleur;
     private $typeCarburant;
     private $numPlaque;
-    private const FORM = "<i><<Rectangulaire>></i>"; //un consate
+    private const FORM = "<i><< Rectangulaire >></i>"; //un consate
     private $err;
     public function __construct($roue, $color, $typC, $num) //constructreur
     {
@@ -31,12 +31,12 @@ class Vehicule
     {
         if($this->nbreRoue >= 4)
         {
-            echo "<b>ce Vehicule est caractvrise par :</b><br>".
+            echo "<BR><BR><b>ce Vehicule est caractvrise par :</b><br>".
                 "Numero du plaque : <span id='nom'><font color='blue'>".$this->numPlaque."</font></span><br>".
                 "Type de Carburant : <span id='nom'><font color='blue'>".$this->typeCarburant."</font></span><br>".
                 "Nombre de roue : <span id='nom'><font color='blue'>".$this->nbreRoue."</font></span><br>".
                 "Couleur : <span id='nom'><font color='blue'>".$this->couleur."</font></span><br>".
-                "avec une Forme ".Vehicule::FORM." "; //Affichge gu constante
+                "avec une Forme ".Vehicule::FORM; //Affichge gu constante
             if($this->nbreRoue >= 4 and $this->nbreRoue <= 6)
                 echo "<b><i><font color='green'> c'est un simple Vehicule</font></i></b>";
             else 
